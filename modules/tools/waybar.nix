325c1008758d0566d81624c611e2d65a7a145ca3
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-04 20:18:44 +0800

    fix waybar module

--- modules/tools/waybar.nix
+++ modules/tools/waybar.nix
@@ -1,17 +1,13 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  # 安裝 Waybar 套件
   home.packages = [
     pkgs.waybar
   ];
 
-  # 管理 Waybar 配置資料夾
-  home.directory.".config/waybar" = {
-    source = ./waybar; # 對應 dotfiles/modules/tools/waybar/waybar 資料夾
+  home.file.".config/waybar" = {
+    source = ./waybar;
+    recursive = true;
   };
-
-  # 選擇性：設定環境變數，例如 Waybar 的 theme 或其他選項
-  # home.sessionVariables.WAYBAR_THEME = "mocha";
 }
 
