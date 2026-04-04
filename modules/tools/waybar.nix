49ef325b09478fc72857a937465a0e3f06260edc
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-04 21:30:05 +0800

    waybar

--- modules/tools/waybar.nix
+++ modules/tools/waybar.nix
@@ -1,10 +1,10 @@
-{
-  home.packages = [
-    pkgs.waybar
-  ];
+{ config, pkgs, ... }:
 
-  xdg.configFile."waybar/config.jsonc".source = ./waybar/config.jsonc;
-  xdg.configFile."waybar/style.css".source = ./waybar/style.css;
-  xdg.configFile."waybar/mocha.css".source = ./waybar/mocha.css;
+{
+  home.packages = [ pkgs.waybar ];
+
+  xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
+  xdg.configFile."waybar/style.css".source = ./style.css;
+  xdg.configFile."waybar/mocha.css".source = ./mocha.css;
 }
 
