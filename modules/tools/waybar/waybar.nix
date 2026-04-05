0bbf85ca854b860edc7026975a92dca7bf60daea
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-05 17:48:04 +0800

    waybar

--- modules/tools/waybar/waybar.nix
+++ modules/tools/waybar/waybar.nix
@@ -1,10 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  waybarHypr = pkgs.waybar.overrideAttrs (old: {
+    mesonFlags = old.mesonFlags ++ [ "-Dhyprland=enabled" ];
+  });
+in
 {
-  home.packages = [ pkgs.waybar ];
+  home.packages = [ waybarHypr ];
 
   xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
   xdg.configFile."waybar/style.css".source = ./style.css;
-  xdg.configFile."waybar/mocha.css".source = ./mocha.css;
+  #xdg.configFile."waybar/mocha.css".source = ./mocha.css;
 }
 
