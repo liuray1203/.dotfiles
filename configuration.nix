ddb69e3fee78fa6b4796c05e59709683840e6f5e
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-06 13:40:52 +0800

    display.nix

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./system/hyprland.nix
+      ./system/display.nix
     ];
 
   # Bootloader.
