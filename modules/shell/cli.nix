09b8162993aff8475d61449d8720dfb56ff6d802
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-12 13:29:44 +0800

    z

--- modules/shell/cli.nix
+++ modules/shell/cli.nix
@@ -13,6 +13,7 @@
     tree
     fzf
     zoxide
+    Distrobox
   ];
 }
 
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -17,6 +17,7 @@
 
       initContent = ''
         eval "$(starship init zsh)"
+	eval "$(zoxide init zsh)"
       '';
     };
 
