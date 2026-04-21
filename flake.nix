c70231be5a7a67f9a859dceeb6f04cb64d91d9db
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-22 06:29:39 +0800

    waybar

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     stylix = { 
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
      };
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
