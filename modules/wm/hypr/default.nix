8ae10e9e50ea65e5f2cb41ed8da3eb1bffc3ad43
liuray <113435@mail.tljh.ylc.edu.tw>
2026-04-26 13:49:54 +0800

    pyprland

--- modules/wm/hypr/default.nix
+++ modules/wm/hypr/default.nix
@@ -7,6 +7,7 @@
     ./input.nix
     ./keybinds.nix
     ./package.nix
+    ./windowrules.nix
   ];
 
   wayland.windowManager.hyprland = {
---
+++ modules/wm/hypr/windowrules.nix
@@ -0,0 +1,10 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    windowrulev2 = [
+      "float,class:^(org.wezfurlong.wezterm)$"
+      "size 80% 50%,class:^(org.wezfurlong.wezterm)$"
+      "center,class:^(org.wezfurlong.wezterm)$"
+    ];
+  };
+}
+
