14aa204df20d6b08016ce5d3d3836893470fa3e9
liuray <113435@mail.tljh.ylc.edu.tw>
2026-05-14 20:59:11 +0800

    use mangowc

--- modules/system/display.nix
+++ modules/system/display.nix
@@ -6,7 +6,67 @@
   services.displayManager.sddm.enable = false;
 
   # 啟用 Mango Compositor (基於 dwl)
-  programs.mango.enable = true;
+  programs.mango = {
+    enable = true;
+    config = ''
+      # --- Autostart ---
+      exec-once = ags
+      exec-once = fcitx5
+      exec-once = swww-daemon
+      exec-once = swww img /home/liuray/.dotfiles/images.jpeg
+
+      # --- Appearance ---
+      blur = 1
+      shadows = 1
+      border_radius = 10
+
+      # --- Keybindings ---
+      # MOD: super, alt, ctrl, shift, none
+
+      # Basic
+      bind = super, return, spawn, kitty
+      bind = super, q, killclient
+      bind = super, space, spawn, bemenu-run
+      bind = super, e, spawn, dolphin
+      bind = super, v, togglefloating
+      bind = super_shift, q, quit
+
+      # Navigation (Vim style)
+      bind = super, h, focusdirection, left
+      bind = super, l, focusdirection, right
+      bind = super, k, focusdirection, up
+      bind = super, j, focusdirection, down
+
+      # Workspaces (Tags)
+      bind = super, 1, view, 1
+      bind = super, 2, view, 2
+      bind = super, 3, view, 3
+      bind = super, 4, view, 4
+      bind = super, 5, view, 5
+      bind = super, 6, view, 6
+
+      # Move to Workspaces
+      bind = super_shift, 1, tag, 1
+      bind = super_shift, 2, tag, 2
+      bind = super_shift, 3, tag, 3
+      bind = super_shift, 4, tag, 4
+      bind = super_shift, 5, tag, 5
+      bind = super_shift, 6, tag, 6
+
+      # Multimedia
+      bind = none, XF86AudioRaiseVolume, spawn, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+      bind = none, XF86AudioLowerVolume, spawn, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      bind = none, XF86AudioMute, spawn, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bind = none, XF86MonBrightnessUp, spawn, brightnessctl -e4 -n2 set 5%+
+      bind = none, XF86MonBrightnessDown, spawn, brightnessctl -e4 -n2 set 5%-
+
+      # Screenshot
+      bind = none, Print, spawn, grim -g "$(slurp)" - | wl-copy
+
+      # Reload
+      bind = super_shift, r, reload
+    '';
+  };
 
   # TTY1 自動啟動 Mango
   # 登入 TTY1 後會自動執行 mango 指令
