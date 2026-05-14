c5b16e5fff7d3fc6035004f39b006ca255513c51
liuray <113435@mail.tljh.ylc.edu.tw>
2026-05-14 19:14:41 +0800

    add AGS

---
+++ config/ags/config.js
@@ -0,0 +1,97 @@
+const audio = await Service.import('audio')
+const network = await Service.import('network')
+const hyprland = await Service.import('hyprland')
+
+const date = Variable("", {
+    poll: [1000, 'date "+%H:%M:%S %b %e"'],
+})
+
+// 工作區組件
+const Workspaces = () => Widget.Box({
+    class_name: "workspaces",
+    children: hyprland.bind("workspaces").as(ws => 
+        ws.sort((a, b) => a.id - b.id).map(({ id }) => Widget.Button({
+            on_clicked: () => hyprland.message(`dispatch workspace ${id}`),
+            child: Widget.Label(`${id}`),
+            class_name: hyprland.active.workspace.bind("id").as(i => 
+                `${i === id ? "focused" : ""}`
+            ),
+        }))
+    ),
+})
+
+const VolumeSlider = () => Widget.Box({
+    class_name: "volume-slider",
+    children: [
+        Widget.Icon({
+            icon: audio.speaker.bind("icon_name"),
+        }),
+        Widget.Slider({
+            hexpand: true,
+            draw_value: false,
+            on_change: ({ value }) => audio.speaker.volume = value,
+            value: audio.speaker.bind("volume"),
+        }),
+    ],
+})
+
+const NetworkIndicator = () => Widget.Icon({
+    icon: network.wifi.bind("icon_name"),
+})
+
+const SystemMenu = () => Widget.Box({
+    vertical: true,
+    class_name: "system-menu",
+    children: [
+        Widget.Box({
+            class_name: "menu-item",
+            children: [Widget.Label("Volume"), VolumeSlider()],
+        }),
+        Widget.Box({
+            class_name: "menu-item",
+            children: [
+                Widget.Label("WiFi: "),
+                Widget.Label({ label: network.wifi.bind("ssid").as(s => s || "Disconnected") }),
+            ],
+        }),
+    ],
+})
+
+const SystemWindow = () => Widget.Window({
+    name: "system-window",
+    class_name: "system-window",
+    anchor: ["top", "right"],
+    visible: false,
+    child: SystemMenu(),
+})
+
+const Bar = (monitor = 0) => Widget.Window({
+    name: `bar-${monitor}`,
+    class_name: "bar",
+    monitor,
+    anchor: ["top", "left", "right"],
+    exclusivity: "exclusive",
+    child: Widget.CenterBox({
+        start_widget: Workspaces(),
+        center_widget: Widget.Label({
+            label: date.bind(),
+        }),
+        end_widget: Widget.Button({
+            on_primary_click: () => App.toggleWindow("system-window"),
+            child: Widget.Box({
+                children: [
+                    NetworkIndicator(),
+                    Widget.Label({ label: " System" }),
+                ],
+            }),
+        }),
+    }),
+})
+
+App.config({
+    style: "./style.css",
+    windows: [
+        Bar(),
+        SystemWindow(),
+    ],
+})
--- modules/desktop/ags/default.nix
+++ modules/desktop/ags/default.nix
@@ -8,10 +8,10 @@
   programs.ags = {
     enable = true;
 
-    # 這裡可以指向你的設定檔目錄
-    # configDir = ../../../config/ags;
+    # 指向 .dotfiles 內的設定檔目錄
+    configDir = ../../../config/ags;
 
-    # 額外需要的套件，例如用於顯示圖標或執行腳本
+    # 額外需要的套件
     extraPackages = with pkgs; [
       gtksourceview
       webkitgtk_6_0
