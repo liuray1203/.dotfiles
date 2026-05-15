1fa25001a6b9f3aef0ab5bb72098c21bcdc4655e
liuray <113435@mail.tljh.ylc.edu.tw>
2026-05-15 18:18:05 +0800

    fix hyprland

--- modules/system/display.nix
+++ modules/system/display.nix
@@ -1,6 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
+  # 啟用硬體繪圖支援 (必要的)
+  hardware.graphics.enable = true;
+
+  # 啟用 Hyprland 系統組件 (處理 Polkit, Portals 等)
+  programs.hyprland.enable = true;
+
+  # 如果你想使用 NVIDIA 顯卡，請取消以下註解：
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.enable = false;
+  #   powerManagement.finegrained = false;
+  #   open = false;
+  #   nvidiaSettings = true;
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
+
   # 停用 X server 與 SDDM (改用 TTY 登入)
   services.xserver.enable = false;
   services.displayManager.sddm.enable = false;
