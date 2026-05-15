901550bdf18edf7686b22b5df5f1fb4311be3e76
liuray <113435@mail.tljh.ylc.edu.tw>
2026-05-16 05:50:55 +0800

    fix chrome

--- modules/system/display.nix
+++ modules/system/display.nix
@@ -7,16 +7,27 @@
   # 啟用 Hyprland 系統組件 (處理 Polkit, Portals 等)
   programs.hyprland.enable = true;
 
-  # 如果你想使用 NVIDIA 顯卡，請取消以下註解：
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   powerManagement.enable = false;
-  #   powerManagement.finegrained = false;
-  #   open = false;
-  #   nvidiaSettings = true;
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+  # 啟用 NVIDIA 專有驅動
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # 配置 PRIME 雙顯卡切換 (Intel + NVIDIA)
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      # 根據 lspci 結果填寫 Bus ID
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
   # 停用 X server 與 SDDM (改用 TTY 登入)
   services.xserver.enable = false;
