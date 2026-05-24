f56e95f1decd2514efafbcc13b2a2d98c69aa53d
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-05-24 17:24:53 +0800

    add upower

--- configuration.nix
+++ configuration.nix
@@ -74,4 +74,25 @@
 
   #加上upower
   services.upower.enable = true;
+
+#加上tlp
+
+  services.tlp = {
+    enable = true;
+
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      WIFI_PWR_ON_BAT = "on";
+
+      USB_AUTOSUSPEND = 1;
+
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
 }
+
