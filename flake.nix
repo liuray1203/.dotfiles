889aca2eb8d02c177e9e871b521232e63574bd0d
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-05-31 21:26:30 +0800

    add upower

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS + Home Manager modular setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
