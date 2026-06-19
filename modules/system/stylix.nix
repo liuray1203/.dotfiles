cab1baf031feec5bdb3a13a98b7df8e5214614bb
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-06-19 17:14:43 +0800

    更改滑鼠游標 nixos26.05 2026 6/19

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,10 +1,20 @@
 { pkgs, ... }:
+
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark.yaml";
-    image = ../../images.jpeg; 
+
+    base16Scheme =
+      "${pkgs.base16-schemes}/share/themes/gruvbox-dark.yaml";
+
+    image = ../../images.jpeg;
     polarity = "dark";
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
   };
 
   stylix.targets.gnome.enable = false;
--- modules/wm/hypr/default.nix
+++ modules/wm/hypr/default.nix
@@ -24,7 +24,10 @@
       ];
 
       env = [
+        "XCURSOR_THEME,Bibata-Modern-Ice"
         "XCURSOR_SIZE,24"
+
+        "HYPRCURSOR_THEME,Bibata-Modern-Ice"
         "HYPRCURSOR_SIZE,24"
       ];
     };
