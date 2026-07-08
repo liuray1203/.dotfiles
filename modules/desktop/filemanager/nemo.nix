6c95114da6f980f8bed429ab5419f8807b5e3fae
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-07-08 20:13:30 +0800

    add papirus-icon-theme nixos26.05 2026 7/8

--- modules/desktop/filemanager/nemo.nix
+++ modules/desktop/filemanager/nemo.nix
@@ -3,7 +3,17 @@
 {
   home.packages = with pkgs; [
     nemo
+    papirus-icon-theme
   ];
+
+   gtk = {
+    enable = true;
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+  };
 
   xdg.mimeApps = {
     enable = true;
