e7e68ef9a1e2a21b243b36888ef87525a520f642
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-07-11 14:53:18 +0800

    加上 圖示 nixos26.05 2026 7/8

--- modules/shell/zsh/starship.nix
+++ modules/shell/zsh/starship.nix
@@ -6,7 +6,7 @@
       settings = {
         add_newline = false;
 
-        format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)$character";
+        format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)$character";
 
         directory = {
           style = "fg:#e3e5e5 bg:#769ff0";
