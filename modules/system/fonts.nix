fb2efd779f3b0aa9055bf3ba7661fa9ab6ffbd98
liuray1203 <113435@mail.tljh.ylc.edu.tw>
2026-08-02 14:03:56 +0800

    fix firefox 8/1 nixos26.05

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -7,8 +7,16 @@
     font-awesome
     noto-fonts
     noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     noto-fonts-color-emoji
     nerd-fonts.meslo-lg 
   ];
+
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = [ "Noto Sans CJK TC" ];
+    serif = [ "Noto Serif CJK TC" ];
+    monospace = [ "Noto Sans Mono CJK TC" ];
+    emoji = [ "Noto Color Emoji" ];
+  };
 }
 
